e2027d4d589e07229cb8f109fd643286b13d39bf
Livsi <livsixiaomi@gmail.com>
2019-12-24 22:04:08 +0300

    Add delimiter to csv strategy

--- output_strategies/csv_strategy.py
+++ output_strategies/csv_strategy.py
@@ -5,8 +5,10 @@ from output_strategies.base_strategy import OutputStrategy
 
 class CSVStrategy(OutputStrategy):
 
+    DELIMITER = ';'
+
     def write_output_data(self, output_filename, result_dict):
         with open(output_filename, 'w', newline='') as file_obj:
-            writer = csv.writer(file_obj)
+            writer = csv.writer(file_obj, delimiter=self.DELIMITER)
             writer.writerow(result_dict.keys())
             writer.writerows(zip(*result_dict.values()))
